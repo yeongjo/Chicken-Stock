82e64a1ef432a9317ec4529f4112cc809e550520
sc2466 <songchung2466@gmail.com>
2020-05-20 02:37:58 +0900

    csv로 종목의 거래량과 시가를 저장함, 아키네이터의 IPC 모듈추가

---
+++ IPC.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import time
+import errno
+import threading
+import pickle
+
+WritePath='pipe\\pipe'
+ReadPath='pipe\\pipe'
+
+class ReceiveThread (threading.Thread):
+
+    def default_receive_callback (recv):
+        print('received - ' + str(recv))
+        return
+
+    recv_callback = default_receive_callback
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run (self):
+        while (True):
+            try:
+                #Read pipe file
+                pipe = open(ReadPath, 'r+b')
+                buffer = pipe.readlines()
+                pipe.truncate(0)
+                pipe.close()
+
+                #Iterate and call callback function
+                bCount = len(buffer)
+                idx=0
+                while (idx < bCount):
+                    received = pickle.loads(buffer[idx])
+                    self.recv_callback(received)
+                    idx+=1
+
+            except OSError as e:
+                if e.errno==11:
+                    continue
+                else:
+                    print('OSError is occurred in ipc thread. You`re fucked up')
+                    break
+
+            except EOFError as e:
+                time.sleep(0.1)
+                continue
+
+            except:
+                print('Unknown is occurred in ipc thread. You`re totally fucked up')
+                break;
+
+            time.sleep(0.1)
+
+        print('Receive thread is stopped')
+        return
+
+def StartReceiving (callback=None):
+    t = ReceiveThread()
+    t.recv_callback = callback
+    t.start()
+    return
+
+def Send(data):
+
+    p = pickle.dumps(data)
+    pipe = open(WritePath, 'ab')
+    p += bytes(os.linesep.encode('utf-8'))
+    pipe.write(p)
+    pipe.close()
+
+    return;
+
+
+#[아키네이터] [2:35] Send는 전송
+#[아키네이터] [2:35] StartReceiving은 수신
+#[아키네이터] [2:35] StartReceiving은 콜백함수 하나 등록해두면 수신받았을때 호출될거야
+#[아키네이터] [2:35] 콜백함수는 파라미터 하나니까 유의하고
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QAxContainer import QAxWidget
 from PyQt5.QtWidgets import QApplication
+import csv
 
 
 class KiwoomAPI:
@@ -14,7 +15,11 @@ class KiwoomAPI:
     def reqTR(self):
         #opt10001
         self.OCXconn.dynamicCall("SetInputValue(QString, QString)", "종목코드", "005930")
-        self.OCXconn.dynamicCall("CommRqData(QString, QString, QString, QString)", "AT_opt10001", "opt10001", "0", "0101")
+        #self.OCXconn.dynamicCall("CommRqData(QString, QString, QString, QString)", "AT_opt10001", "opt10001", "0", "0101") # 이름출력
+
+        self.OCXconn.dynamicCall("SetInputValue(QString, QString)", "틱범위", "1:1분")
+        self.OCXconn.dynamicCall("SetInputValue(QString, QString)", "수정주가구분", "1")
+        self.OCXconn.dynamicCall("CommRqData(QString, QString, QString, QString)", "주식분봉차트조회요청", "OPT10080", "0", "1000")
 
     def login(self):
         ret = self.OCXconn.dynamicCall("CommConnect()")
@@ -29,14 +34,29 @@ class KiwoomAPI:
 
     def trEvent(self, sScrNo, sRQName, sTrCode, sRecordName, sPreNext, nDataLength, sErrorCode, sMessage, sSplmMsg):
         # sScrNo(화면번호), sRQName(사용자구분), sTrCode(Tran명), sRecordName(레코드명), sPreNext(연속조회 유무)
-        if sRQName == 'AT_opt10001':
+        if sRQName == 'AT_opt10001': # 이름 출력
             print('레코드이름', sRecordName)
-            name = self.OCXconn.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, 0, "종목명")
-            print(name.strip())
+            name = self.OCXconn.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, 0, "종목명").strip()
+            print(name)
+
+        if sRQName == '주식분봉차트조회요청':
+            with open('test.csv','w', newline='') as csvfile:
+                spamwriter = csv.writer(csvfile)
+                dataCnt = self.OCXconn.dynamicCall("GetRepeatCnt(QString, QString)", sTrCode, sRQName);
+                print('데이터 계속여부 : ', sPreNext, ', 가져온 갯수 : ', dataCnt)
+                for i in range(100):
+                    date = self.OCXconn.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "체결시간").strip()
+                    start = int(self.OCXconn.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "시가").strip())
+                    mount = self.OCXconn.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "거래량").strip()
+                    print(i," :{0}, {1}, {2}".format(date, start, mount))
+
+                    spamwriter.writerow([start, mount])
 
 
+                               
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     test = KiwoomAPI()
     test.login()
     app.exec_()
+    
