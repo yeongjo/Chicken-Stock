57f3ff6a3c16f12f41bbca28fda095230b9ebc59
2DGP-Yum <yumtr@naver.com>
2020-06-11 02:32:49 +0900

    ff

--- test.py
+++ test.py
@@ -23,35 +23,52 @@ class MyWindow(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.initUI()
-        self.setLayout(self.layout)
-        self.setGeometry(200, 200, 800, 600)
-
-    def initUI(self):
         self.fig = plt.Figure()
         self.canvas = FigureCanvas(self.fig)
+        self.lb1 = QLabel()
+        self.pb1 = QPushButton()
+        self.pb2 = QPushButton()
+        self.layout = QBoxLayout(QVBoxLayout.BottomToTop, self)
+        self.setLayout(self.layout)
+        self.setGeometry(200, 200, 800, 600)
+        self.initUI()
+        self.x = 100
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.canvas)
+    def initUI(self):
 
-        pybutton = QPushButton('한강위치', self)
-        pybutton.resize(100, 32)
-        pybutton.move(100, 320)
+        self.lb1.setText("테스트")
+        self.pb1.setText("한강위치")
+        self.pb1.resize(110, 312)
+        self.pb2.setText("test")
+        print(self.pb1.size())
+        self.pb1.move(0, 1320)
+        print(self.pb1.size())
         # 클릭 이벤트 연결
-        pybutton.clicked.connect(Map)
-        layout.addWidget(pybutton)
+        self.pb1.clicked.connect(Map)
+        self.pb2.clicked.connect(self.testdef)
+        self.layout.addWidget(self.lb1, alignment=Qt.AlignCenter)
+        self.layout.addWidget(self.pb1, alignment=Qt.AlignLeft)
+        self.layout.addWidget(self.pb2, alignment=Qt.AlignRight)
 
-        self.layout = layout
+        self.test = [132, 423, 123, 442, 3132, 1132, 423, 1412, 323]
         self.test_graph()
 
+
     def test_graph(self):
-        test = [132, 423, 123, 442, 3132, 1132, 423, 1412, 323]
         test2 = [232, 1123, 2323, 2342, 1432, 532, 423, 412, 623]
         self.fig.clear()
-        ax = self.fig.add_subplot(2, 1, 1)
-        ax.plot(test)
-        ax.plot(test2)
+        ax = self.fig.add_subplot(3, 1, 1)
+        ax2 = self.fig.add_subplot(3, 1, 2)
+        ax.plot(self.test)
+        ax2.plot(test2)
         self.canvas.draw()
+        self.layout.addWidget(self.canvas)
+
+    def testdef(self):
+        self.x -= 1000
+        self.test.append(self.x)
+        self.test_graph()
+
 
 
 if __name__ == '__main__':
